3190edacdc0974d9db51135dd6fcafdc0c003702
Agbell <agbell@gmail.com>
2014-04-26 02:05:24 +0000

    multigroups

--- src/Util.hs
+++ src/Util.hs
@@ -7,12 +7,23 @@ import qualified Data.Map
 
 merge :: [Note] -> [Note] -> [Note]
 merge n1 n2 = fromMap $ Data.Map.union (toMap n2) (toMap n1) 
-
-toMap :: [Note] -> Data.Map.Map Version Note
-toMap notes = Data.Map.fromList $ fmap (\x -> (version x, x)) notes
-
-fromMap :: Data.Map.Map Version Note -> [Note]
-fromMap map = Data.Map.elems map
+ where
+  toMap :: [Note] -> Data.Map.Map Version Note
+  toMap notes = Data.Map.fromList $ fmap (\x -> (version x, x)) notes
+  fromMap :: Data.Map.Map Version Note -> [Note]
+  fromMap map = reverse $ Data.Map.elems map
 
 group :: [Note] -> [Group]
-group ns = [ Group { from = Version 3 0 0 0, to = Version 4 0 0 0, notes = ns}]
+group ns = fmap (fillGroup ns) Util.groups
+ 
+fillGroup :: [Note] -> Group -> Group
+fillGroup ns (Group f t _) = Group f t ns1
+ where
+    ns1 = (Prelude.filter (\ (Note v _ _) -> f <= v && v <= t) ns)
+
+groups = reverse [ 
+    Group { from = Version 3 0 10 70, to = Version 3 0 10 79, notes = []},
+    Group { from = Version 3 0 10 80, to = Version 3 0 10 89, notes = []},
+    Group { from = Version 3 0 10 90, to = Version 3 0 10 99, notes = []}
+    ]
+
