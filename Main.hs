39f26850f5e30f55863c40e25ed01712289d54e1
Agbell <agbell@gmail.com>
2014-04-26 02:35:53 +0000

    groups working

--- Main.hs
+++ Main.hs
@@ -8,14 +8,13 @@ import Render
 ----------------------
 -------- MAIN --------
 ----------------------
-
 main = go1 --Parse.testDate
 
 
 go1 = do
-    n <- Parse.notes "override_notes.log"
+    n <- Parse.notes "notes.log"
     n1 <- Parse.notes "override_notes.log"
     ns <-  return $ merge n n1
-    grp <- return $ group ns
+    grp <- return $ group 10 ns
     html <- render "notesGroup.html" grp
     print html
--- src/Util.hs
+++ src/Util.hs
@@ -3,6 +3,7 @@
 module Util where
 
 import Data
+import qualified Data.List
 import qualified Data.Map
 
 merge :: [Note] -> [Note] -> [Note]
@@ -13,17 +14,17 @@ merge n1 n2 = fromMap $ Data.Map.union (toMap n2) (toMap n1)
   fromMap :: Data.Map.Map Version Note -> [Note]
   fromMap map = reverse $ Data.Map.elems map
 
-group :: [Note] -> [Group]
-group ns = fmap (fillGroup ns) Util.groups
+group :: Int -> [Note] -> [Group]
+group groupSize ns = fmap (fillGroup ns) (generateGroups groupSize ns)
  
 fillGroup :: [Note] -> Group -> Group
 fillGroup ns (Group f t _) = Group f t ns1
  where
     ns1 = (Prelude.filter (\ (Note v _ _) -> f <= v && v <= t) ns)
 
-groups = reverse [ 
-    Group { from = Version 3 0 10 70, to = Version 3 0 10 79, notes = []},
-    Group { from = Version 3 0 10 80, to = Version 3 0 10 89, notes = []},
-    Group { from = Version 3 0 10 90, to = Version 3 0 10 99, notes = []}
-    ]
-
+generateGroups :: Int -> [Note] -> [Group]
+generateGroups groupSize ns = Data.List.nub $ fmap (range . version) ns
+ where 
+     range (Version m1 m2 b r) = Group { from = Version m1 m2 b (floor r), to = Version m1 m2 b (ceil r), notes = []}
+     floor r = r `div` groupSize * groupSize
+     ceil r = r `div` groupSize * groupSize + groupSize -1
