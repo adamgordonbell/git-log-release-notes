bf8b756470bb3a8db963689e09064ece6614fb72
Adam Bell <adam.bell@diamondmunicipal.com>
2014-04-28 15:41:52 -0400

    remove original parser

--- Main.hs
+++ Main.hs
@@ -2,7 +2,6 @@
 {-# LANGUAGE NoMonomorphismRestriction #-}
 
 import qualified ReleaseNotes.Parse as Parse
-import qualified ReleaseNotes.Parse2 as Parse2
 import ReleaseNotes.Data
 import ReleaseNotes.Util
 import ReleaseNotes.Render
@@ -14,21 +13,11 @@ import qualified Data.Text.IO as Text
 ----------------------
 -------- MAIN --------
 ----------------------
-main = go2 --Parse2.testLine -- go2 --Parse.testDate
+main = go1 --Parse2.testLine -- go2 --Parse.testDate
 
-
-go1 = do
-    n <- Parse.notes "notes.log"
-    n1 <- Parse.notes "override_notes.log"
-    ns <-  return $ merge n n1
-    grp <- return $ group 10 ns
-    html <- render "notesGroup.html" grp
-    Text.putStrLn  html
-    Text.writeFile "releaseNotes.html" html
-
-go2 = do 
-      n <- fmap convertLines $ Parse2.lines "notes_complicated.log"
-      n1 <- fmap convertLines $ Parse2.lines "override_notes.log"
+go1 = do 
+      n <- fmap convertLines $ Parse.lines "notes_complicated.log"
+      n1 <- fmap convertLines $ Parse.lines "override_notes.log"
       ns <-  return $ merge n n1
       grp <- return $ group 10 ns
       html <- render "notesGroup.html" grp
